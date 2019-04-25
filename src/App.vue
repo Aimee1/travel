6601422875574a2ffdf547f7979bc483629409a4
jr <183901910@qq.com>
2019-04-25 17:55:16 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <!--显示的当前路由地址所对应的内容-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,20 @@
+
+<template>
+<div>
+    <HomeHeader></HomeHeader>
+</div>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+
+export default {
+  name: 'home',
+  components:{
+       HomeHeader:HomeHeader
+   }
+}
+</script>
+
+<style>
+</style>
